63df2f2a8402b4c09ea900bcf369cfb08af138d1
Damon Kelley <damon.kelley@gmail.com>
2020-07-31 16:38:09 -0400

    Introduce the SummarizedLink

--- src/main/kotlin/com/damonkelley/publinked/Link.kt
+++ src/main/kotlin/com/damonkelley/publinked/Link.kt
@@ -1,10 +1,24 @@
 package com.damonkelley.publinked
 
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.Fetch
+import org.hibernate.annotations.FetchMode
 import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.Immutable
+import org.hibernate.annotations.Subselect
+import org.hibernate.annotations.Synchronize
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import java.util.Date
+import java.util.Optional
+import javax.persistence.CascadeType
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.OneToMany
+import javax.persistence.Table
 
 @Entity
 data class Link(
@@ -12,7 +26,63 @@ data class Link(
     @GeneratedValue(generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid")
     val id: String? = null,
-    val href: String? = null
+
+    val href: String? = null,
+
+    @OneToMany(cascade = [CascadeType.ALL])
+    @Fetch(FetchMode.JOIN)
+    val activities: MutableList<Activity> = mutableListOf(),
+
+    @field:CreationTimestamp
+    val createdDate: Date? = null
+)
+
+@Entity
+data class Activity(
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    val id: String? = null,
+    val name: String,
+
+    @field:CreationTimestamp
+    val createdDate: Date? = null
+)
+
+@Entity
+@Immutable
+@Subselect(
+    """
+    (SELECT
+         l.id as link_id, a.name as name, count(a.*) as count from link l
+         JOIN link_activities la on l.id = la.link_id
+         JOIN activity a on la.activities_id = a.id
+         GROUP BY l.id, a.name)
+"""
+)
+@Synchronize("Link", "Activity")
+data class ActivitySummary(
+    var linkId: String? = "",
+    @Id
+    var name: String? = "",
+    var count: Long? = 0L
+)
+
+@Entity
+@Immutable
+@Table(name = "link")
+@Synchronize("Link", "Activity")
+data class SummarizedLink(
+    val href: String,
+    @Id
+    val id: String,
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "linkId")
+    val summary: List<ActivitySummary>
 )
 
 interface LinkRepository : CrudRepository<Link, String>
+interface SummarizedLinkRepository : CrudRepository<SummarizedLink, String> {
+    @Query("FROM SummarizedLink link JOIN FETCH link.summary")
+    override fun findById(id: String): Optional<SummarizedLink>
+}
---
+++ src/test/kotlin/com/damonkelley/publinked/LinkRepositoryTest.kt
@@ -0,0 +1,39 @@
+package com.damonkelley.publinked
+
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.collections.singleElement
+import io.kotest.matchers.shouldHave
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+
+@SpringBootTest
+class LinkRepositoryTest(@Autowired val repository: LinkRepository, @Autowired val summarizedLinkRepository: SummarizedLinkRepository) {
+    @Test
+    fun `it will save and fetch the associated activities`() {
+        val link = repository.save(
+            Link(href = "http://example.com").apply {
+                activities += Activity(name = "followed")
+            }
+        )
+
+        link.activities shouldHave singleElement { it.name == "followed" }
+    }
+
+    @Test
+    fun `it will count the number of followed activities`() {
+        val link = repository.save(
+            Link(href = "http://example.com").apply {
+                activities += Activity(name = "followed")
+                activities += Activity(name = "audited")
+            }
+        )
+
+        val summarizedLink = summarizedLinkRepository.findByIdOrNull(link.id!!)!!
+
+        val summary = summarizedLink.summary.map { it.name to it.count }
+
+        summary shouldContainAll listOf("followed" to 1L, "audited" to 1L)
+    }
+}
